037694a527441ff9d71e686b234014284c44b6c4
abdAlhadiM <58364677+abdAlhadiM@users.noreply.github.com>
2019-12-17 20:12:07 +0300

    Update AVLTree.cs

--- Collections/AVLTree.cs
+++ Collections/AVLTree.cs
@@ -1,15 +1,38 @@
 using System;
+using System.Linq;
 using System.Collections;
+using System.Diagnostics;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
-public class AVLTree<T> :
-     IEnumerable<T>,
-     IEnumerable,
-     ICollection<T>,
-     ICollection,
-     IReadOnlyCollection<T>
+internal static class SerializationStreamExtensions
 {
-    private class Node
+    public static void AddValue<T>(this SerializationInfo @this, string name, T val)
+    {
+        @this.AddValue(name, val, typeof(T));
+    }
+}
+
+public interface INode<T>
+{
+    INode<T> Left { get; }
+    INode<T> Right { get; }
+    INode<T> Parent { get; }
+    int Height { get; }
+    int Frequency { get; }
+    T Val { get; }
+}
+
+[Serializable]
+public class AVLTree<T> : IReadOnlyCollection<T>,
+                          ICollection,
+                          ICollection<T>,
+                          IEnumerable<T>,
+                          IEnumerable,
+                          ISerializable
+{
+
+    private class Node : INode<T>
     {
         public Node left;
         public Node right;
@@ -18,164 +41,119 @@ public class AVLTree<T> :
         public int frequency;
         public T val;
 
-        public Node(T val)
+        public int Height => height;
+        public int Frequency => frequency;
+        public T Val => val;
+
+        public INode<T> Left => left;
+        public INode<T> Right => right;
+        public INode<T> Parent => parent;
+         
+        public Node() { }
+        public Node(T val, Node parent = null)
         {
             this.val = val;
-            parent = right = left = null;
+            this.parent = parent;
             height = 0;
             frequency = 1;
+            left = right = null;
         }
+
 
         public Node Any() => left ?? right;
         public bool IsFull() => left != null && right != null;
         public bool IsLeaf() => left is null && right is null;
-        public bool HasChildren() => left != null && right != null;
+        public bool HasChildren() => left != null || right != null;
 
         public static implicit operator bool(Node node) => node != null;
     }
-    public bool IsSynchronized => false;
+
+    [NonSerialized]
+    private object _syncRoot;
     public bool IsReadOnly => false;
+    public bool IsSynchronized => false;
     public object SyncRoot
     {
         get
         {
             if (_syncRoot == null)
             {
-                System.Threading.Interlocked.CompareExchange(ref _syncRoot, new object(), 0);
+                _syncRoot = System.Threading.Interlocked.CompareExchange(ref _syncRoot, new object(), null);
             }
             return _syncRoot;
         }
     }
-
-    public IComparer<T> Comparer => comparer;
-    public int Count { get; private set; }
-
+    private readonly IComparer<T> comparer;
     private Node root;
-    private object _syncRoot;
-    private IComparer<T> comparer;
-
+    public int Count { get; private set; }
 
     public AVLTree()
     {
         comparer = Comparer<T>.Default;
     }
-
     public AVLTree(IComparer<T> comparer)
     {
         this.comparer = comparer ?? Comparer<T>.Default;
     }
 
-    private int Height(Node node) => node == null ? -1 : node.height;
-    private void UpdateHeight(Node node)
-    {
-        while (node)
-        {
-            node.height = 1 + Math.Max(Height(node.left), Height(node.right));
-            node = node.parent;
-        }
-    }
-    private int BalanceFactor(Node node) => Height(node.left) - Height(node.right);
+    public AVLTree(IEnumerable<T> collection) : this(collection, Comparer<T>.Default) { }
 
-    private void Balance(Node node, bool isLeft)
+    public AVLTree(IEnumerable<T> collection, IComparer<T> comparer) : this(comparer)
     {
-        while (node)
+        if (collection is null)
         {
-            var bf = BalanceFactor(node);
-            var old = node;
-            //case right heavy
-            if (bf < -1)
+            throw new ArgumentNullException(nameof(collection));
+        }
+
+        if (collection is AVLTree<T> other && comparer.Equals(other.comparer))
+        {
+            if (other.Count == 0)
             {
-                //RL
-                if (isLeft)
+                root = null;
+                Count = 0;
+            }
+        }
+        else
+        {
+            Dictionary<T, int> map = new Dictionary<T, int>();
+            foreach (var item in collection)
+            {
+                if (!map.ContainsKey(item))
                 {
-                    node = RotateRight(node.right);
-                    UpdateHeight(node);
-                    node = RotateLeft(node.parent);
-                    UpdateHeight(node);
+                    map.Add(item, 1);
                 }
-                //RR
                 else
                 {
-                    node = RotateLeft(node);
-                    UpdateHeight(node);
+                    map[item]++;
                 }
             }
-            //case left heavy
-            else if (bf > 1)
-            {
-                //LL
-                if (isLeft)
-                {
-                    node = RotateRight(node);
-                    UpdateHeight(node);
-                }
-                //LR
-                else
-                {
-                    node = RotateLeft(node.left);
-                    UpdateHeight(node);
-                    node = RotateRight(node.parent);
-                    UpdateHeight(node);
-                }
-            }
-            if (old.parent) isLeft = old == old.parent.left;
-            node = node.parent;
+            var freq = map.Values.ToArray();
+            var keys = map.Keys.ToArray();
+            Array.Sort(keys);
+            root = BuildFromSortedArray(keys, freq, 0, keys.Length - 1, null);
+            Count = keys.Length;
         }
     }
 
-    private Node RotateLeft(Node node)
+    private Node BuildFromSortedArray(T[] values, int[] freq, int l, int r, Node parent)
     {
-        var other = node.right;
+        if (l > r) return null;
 
-        if (other)
-        {
-            node.right = other.left;
-            node.height = 1 + Math.Max(Height(node.left), Height(node.right));
+        int mid = l + (r - l) / 2;
 
-            if (other.left) other.left.parent = node;
-            other.parent = node.parent;
-            other.left = node;
-        }
-
-        if (!node.parent) root = other;
-        else if (node == node.parent.left) node.parent.left = other;
-        else node.parent.right = other;
-
-        node.parent = other;
-
-        return other;
-    }
-
-    private Node RotateRight(Node node)
-    {
-        var other = node.left;
-
-        if (other)
-        {
-            node.left = other.right;
-            node.height = 1 + Math.Max(Height(node.left), Height(node.right));
-
-            if (other.right) other.right.parent = node;
-            other.parent = node.parent;
-            other.right = node;
-        }
-
-
-        if (!node.parent) root = other;
-        else if (node == node.parent.left) node.parent.left = other;
-        else node.parent.right = other;
-
-        node.parent = other;
-        return other;
+        var current = new Node(values[mid], parent);
+        current.left = BuildFromSortedArray(values, freq, l, mid - 1, current);
+        current.right = BuildFromSortedArray(values, freq, mid + 1, r, current);
+        current.height = 1 + Math.Max(Height(current.left), Height(current.right));
+        current.frequency = freq[mid];
+        return current;
     }
 
     public void Add(T val)
     {
-        var node = new Node(val);
-
         if (root == null)
         {
-            root = node;
+            root = new Node(val);
             Count++;
             return;
         }
@@ -189,7 +167,7 @@ public class AVLTree<T> :
             {
                 if (!current.right)
                 {
-                    current.right = node;
+                    current.right = new Node(val, parent: current);
                     break;
                 }
                 current = current.right;
@@ -198,7 +176,7 @@ public class AVLTree<T> :
             {
                 if (!current.left)
                 {
-                    current.left = node;
+                    current.left = new Node(val, parent: current);
                     break;
                 }
                 current = current.left;
@@ -210,27 +188,118 @@ public class AVLTree<T> :
                 return;
             }
         }
-
-        node.parent = current;
         Count++;
-        UpdateHeight(current);
-        Balance(current, node == current.left);
+        Balance(current);
     }
+
+    private void Balance(Node node)
+    {
+        while (node)
+        {
+            node.height = 1 + Math.Max(Height(node.left), Height(node.right));
+            var bf = BalanceFactor(node);
+
+            //L
+            if (bf < -1)
+            {
+                //LL
+                if (BalanceFactor(node.left) <= 0)
+                {
+                    node = RotateRight(node);
+                }
+                //LR
+                else
+                {
+                    node = RotateLeft(node.left);
+                    node = RotateRight(node.parent);
+                }
+            }
+            //R
+            else if (bf > 1)
+            {
+                //RR
+                if (BalanceFactor(node.right) >= 0)
+                {
+                    node = RotateLeft(node);
+                }
+                //RL
+                else
+                {
+                    node = RotateRight(node.right);
+                    node = RotateLeft(node.parent);
+                }
+            }
+
+
+            node = node.parent;
+        }
+    }
+
+    private Node RotateLeft(Node node)
+    {
+        var other = node.right;
+
+        Debug.Assert(other != null);
+
+        node.right = other.left;
+        node.height = 1 + Math.Max(Height(node.left), Height(node.right));
+
+        if (other.left) other.left.parent = node;
+
+        other.parent = node.parent;
+        other.left = node;
+        other.height = 1 + Math.Max(Height(other.left), Height(other.right));
+
+
+        if (!node.parent) root = other;
+        else if (node == node.parent.left) node.parent.left = other;
+        else node.parent.right = other;
+
+        node.parent = other;
+        return other;
+    }
+
+    private Node RotateRight(Node node)
+    {
+        var other = node.left;
+
+        Debug.Assert(other != null);
+
+        node.left = other.right;
+        node.height = 1 + Math.Max(Height(node.left), Height(node.right));
+
+        if (other.right) other.right.parent = node;
+
+        other.parent = node.parent;
+        other.right = node;
+        other.height = 1 + Math.Max(Height(other.left), Height(other.right));
+
+
+        if (!node.parent) root = other;
+        else if (node == node.parent.left) node.parent.left = other;
+        else node.parent.right = other;
+
+        node.parent = other;
+        return other;
+    }
+
+    private int Height(Node node) => node is null ? -1 : node.height;
+    private int BalanceFactor(Node node) => Height(node.right) - Height(node.left);
+
+    public bool Contains(T val)
+    {
+        return GetNode(val);
+    }
+
     public bool Remove(T val)
     {
-        var node = Get(val);
+        var node = GetNode(val);
 
-        if (!node)
-            return false;
+        if (!node) return false;
 
-        if (--Count == 0)
-        {
-            root = null;
-            return true;
-        }
-        if (--node.frequency > 0)
-            return true;
+        Count--;
 
+        if (--node.frequency > 0) return true;
 
         if (node.IsFull())
         {
@@ -239,10 +308,29 @@ public class AVLTree<T> :
             node.frequency = min.frequency;
             node = min;
         }
+        else if (node == root)
+        {
+            if (node.IsLeaf()) root = null;
+            else
+            {
+                var child = node.Any();
+                child.parent = null;
+                root = child;
+            }
+            return true;
+        }
 
+        RemoveInternal(node);
+        Balance(node.parent);
+        return true;
+    }
+
+    private void RemoveInternal(Node node)
+    {
+        var isLeft = node == node.parent.left;
         if (node.IsLeaf())
         {
-            if (node == node.parent.left)
+            if (isLeft)
             {
                 node.parent.left = null;
             }
@@ -250,41 +338,36 @@ public class AVLTree<T> :
             {
                 node.parent.right = null;
             }
-            UpdateHeight(node.parent);
         }
         else
         {
             var child = node.Any();
-            if (node == root)
-            {
-                root = child;
-                child.parent = null;
-                UpdateHeight(root);
-                return true;
-            }
-            var isLeft = node == node.parent.left;
             child.parent = node.parent;
             if (isLeft)
             {
                 node.parent.left = child;
+
             }
             else
             {
                 node.parent.right = child;
             }
-            UpdateHeight(node.parent);
-            Balance(node.parent, !isLeft);
         }
-
-        return true;
     }
 
-    public bool Contains(T val)
+    public T Max()
     {
-        return Get(val);
+        if (!root) throw new InvalidOperationException();
+        return GetMax(root).val;
     }
 
-    private Node Get(T val)
+    public T Min()
+    {
+        if (!root) throw new InvalidOperationException();
+        return GetMin(root).val;
+    }
+
+    private Node GetNode(T val)
     {
         var current = root;
         while (current)
@@ -299,14 +382,17 @@ public class AVLTree<T> :
 
     private Node GetMin(Node node)
     {
+        Debug.Assert(node != null);
         while (node.left)
         {
             node = node.left;
         }
         return node;
     }
+
     private Node GetMax(Node node)
     {
+        Debug.Assert(node != null);
         while (node.right)
         {
             node = node.right;
@@ -314,18 +400,25 @@ public class AVLTree<T> :
         return node;
     }
 
-    public T GetMin()
+    public void CopyTo(T[] array, int arrayIndex)
     {
-        if (root is null)
+        if (array is null)
+        {
+            throw new ArgumentNullException(nameof(array));
+        }
+        if (arrayIndex < 0 || arrayIndex >= array.Length)
+        {
+            throw new InvalidOperationException(nameof(arrayIndex));
+        }
+        if (array.Length - arrayIndex < Count)
+        {
             throw new InvalidOperationException();
-        return GetMin(root).val;
-    }
+        }
 
-    public T GetMax()
-    {
-        if (root is null)
-            throw new InvalidOperationException();
-        return GetMax(root).val;
+        foreach (var item in this)
+        {
+            array[arrayIndex++] = item;
+        }
     }
 
     public void CopyTo(Array array, int index)
@@ -334,31 +427,41 @@ public class AVLTree<T> :
         {
             throw new InvalidOperationException();
         }
+
         if (!(array is T[] arr))
         {
             throw new ArrayTypeMismatchException();
         }
-        CopyTo(arr, index);
-    }
 
-    public void CopyTo(T[] array, int arrayIndex)
-    {
-        if (array == null)
-            throw new ArgumentNullException();
-        if (arrayIndex < 0 || arrayIndex >= array.Length)
-            throw new InvalidOperationException();
-        if (array.Length - arrayIndex < Count)
-            throw new InvalidOperationException();
-        foreach (var item in this)
-        {
-            array[arrayIndex++] = item;
-        }
+        CopyTo(arr, index);
     }
 
     public void Clear()
     {
         root = null;
         Count = 0;
+    }
+     
+    public IEnumerable<IEnumerable<T>> Levels() => Levels((node) => node.Val);
+
+    public IEnumerable<IEnumerable<V>> Levels<V>(Func<INode<T>, V> query)
+    {
+        if (!root) yield break;
+        var queue = new Queue<Node>();
+        queue.Enqueue(root);
+        while (queue.Count != 0)
+        {
+            var level = new List<V>();
+            var n = queue.Count;
+            while (n-- > 0)
+            {
+                var current = queue.Dequeue();
+                if (current.left) queue.Enqueue(current.left);
+                if (current.right) queue.Enqueue(current.right);
+                level.Add(query(current));
+            }
+            yield return level;
+        }
     }
 
     public IEnumerator<T> GetEnumerator()
@@ -368,38 +471,53 @@ public class AVLTree<T> :
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-    private class InorderEnumerator
-        : IEnumerator<T>,
-          IEnumerator,
-          IEnumerable<T>,
-          IEnumerable
+    protected AVLTree(SerializationInfo info, StreamingContext context)
     {
-        private readonly AVLTree<T> avlTree;
-        private readonly Stack<Node> stack;
-        private readonly int threadId;
+    }
+
+    void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+    }
+
+    private struct InorderEnumerator : IEnumerator<T>,
+                                       IEnumerator,
+                                       IEnumerable<T>,
+                                       IEnumerable
+    {
+        private AVLTree<T> avlTree;
+        private Stack<Node> stack;
+        private readonly int ThreadId;
         private Node root;
         private int freq;
         public InorderEnumerator(AVLTree<T> avlTree)
         {
-
+            Debug.Assert(avlTree != null);
             this.avlTree = avlTree;
+            stack = new Stack<Node>((int)Math.Log2(avlTree.Count + 1));
+            ThreadId = Environment.CurrentManagedThreadId;
             root = avlTree.root;
-            stack = new Stack<Node>();
-            threadId = Environment.CurrentManagedThreadId;
             freq = 0;
+            Current = default;
+        }
+
+        public T Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            avlTree = null;
+            stack = null;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            if (threadId != Environment.CurrentManagedThreadId)
+            if (ThreadId != Environment.CurrentManagedThreadId)
                 return new InorderEnumerator(avlTree);
             return this;
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        public T Current { get; private set; }
-        object IEnumerator.Current => Current;
 
         public bool MoveNext()
         {
@@ -407,12 +525,16 @@ public class AVLTree<T> :
             {
                 return true;
             }
+
             while (root)
             {
                 stack.Push(root);
                 root = root.left;
             }
-            if (stack.Count == 0) return false;
+
+            if (stack.Count == 0)
+                return false;
+
             root = stack.Pop();
             Current = root.val;
             freq = root.frequency;
@@ -423,10 +545,8 @@ public class AVLTree<T> :
         public void Reset()
         {
             root = avlTree.root;
-        }
-
-        public void Dispose()
-        {
+            stack.Clear();
+            freq = 0;
         }
     }
 }
